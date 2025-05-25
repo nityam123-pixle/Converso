39db1feda48c7c81248832fd76939156418f1dd2
Nityam Suchak <apple@Nityams-MacBook-Air.local>
2025-05-25 17:31:37 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <h1 className='text-2xl underline'>Hello World welcome to my Saas application</h1>
+        <Button>Let's get started</Button>
+    </div>
+  )
+}
+
+export default Page
