0e78874aecb0ab5481edf822ae3c2d74e23700f3
Nityam Suchak <apple@Nityams-MacBook-Air.local>
2025-05-25 17:51:07 +0530

    Implemented simple navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from '@/components/ui/button'
+import Navbar from '@/components/navbar'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-        <h1 className='text-2xl underline'>Hello World welcome to my Saas application</h1>
-        <Button>Let's get started</Button>
-    </div>
+    <Navbar />
   )
 }
 
