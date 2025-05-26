ee6f26570f00f03aeccc2f82ae28dd0417e1d619
Nityam Suchak <apple@Nityams-MacBook-Air.local>
2025-05-26 21:39:44 +0530

    Added the new companion add form and the basic for the billing page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import NavItems from './Navitems'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
@@ -17,7 +18,15 @@ const Navbar = () => {
             </div>
         </Link>
         <div className='flex items-center gap-8'>
-            <NavItems /> 
+            <NavItems />
+            <SignedOut>
+                <SignInButton>
+                    <button className='btn-signin'>Sign In</button>
+                </SignInButton>
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
         </div>
     </nav>
   )
